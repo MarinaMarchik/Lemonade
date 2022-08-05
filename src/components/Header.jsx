83b47c495f6159f744de4de7fa0c6877f9949680
MarinaMarchik <marina-marchik@yandex.ru>
2022-08-05 20:41:07 +0300

    added css files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
         <header className='header'>
-            <img src="./../images/kisspng-when-life-gives-you-lemons-make-lemonade-drawing-vector-painted-lemon-wordart-5a934d433a96e0.66385173151960301124.png" alt="logo"/>Lemonade
+            <img src="https://st.depositphotos.com/2397945/3869/v/450/depositphotos_38693215-stock-illustration-glass-with-lemonade-and-lemons.jpg" alt="logo"/>
+            <div>Lemonade</div>
         </header>
     );
 }
