67f1679c0d757f56c7878607b3b7878e8787f7e5
MarinaMarchik <marina-marchik@yandex.ru>
2022-08-05 11:35:20 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="./../images/kisspng-when-life-gives-you-lemons-make-lemonade-drawing-vector-painted-lemon-wordart-5a934d433a96e0.66385173151960301124.png" alt="logo"/>Lemonade
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+      <div className='content'>
+          <div>
+              <img src="https://slon.fr/wp-content/uploads/2020/05/322s.jpg" alt=""/>
+          </div>
+          <div>
+              <img src="https://pesikmal.ru/wp-content/uploads/2018/10/%D0%A4%D0%BE%D1%82%D0%BE0.jpg" alt=""/>
+          </div>
+          <div>
+              my post
+              <div>
+                  new post
+              </div>
+          </div>
+          <div>
+              <div>post 1</div>
+              <div>post 2</div>
+          </div>
+      </div>
+  );
+}
+
+export default Profile;
