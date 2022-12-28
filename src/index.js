e19cda40adb159528f1845566bcb5c98d542076d
MarinaMarchik <marina-marchik@yandex.ru>
2022-12-28 20:20:40 +0300

    added stateCopy

--- src/index.js
+++ src/index.js
@@ -6,25 +6,20 @@ import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 import './index.css';
 import {Provider} from "react-redux" ;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-let rerenderEntireTree = () => {
-    root.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <Provider store={store}>
-                    <App />
-                </Provider>
-            </BrowserRouter>
-        </React.StrictMode>
-    );
-}
 
-rerenderEntireTree(store.getState());
-store.subscribe(() => {
-    let state = store.getState();
-    rerenderEntireTree(state);
-});
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
